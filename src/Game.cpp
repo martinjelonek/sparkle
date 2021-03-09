3296847eaf0ce186dfffde04ccc676cddec886c6
mj <jelonek.martin@gmail.com>
2021-03-09 16:38:54 +0100

    Adding Constants file and Game.cpp / .h files.

---
+++ src/Game.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "./Constants.h"
+#include "./Game.h"
+
+Game::Game() {
+    this->isRunning = false;
+}
+
+Game::~Game() {
+}
+
+bool Game::IsRunning() const {
+    return this->isRunning;
+}
+
+float projectilePosX = 0.0f;
+float projectilePosY = 0.0f;
+float projectileVelX = 50.0f;
+float projectileVelY = 50.0f;
+
+void Game::Initialize(int width, int height) {
+    //initialize SDL
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "Error initializing SDL." << std::endl;
+        return;
+    }
+    
+    //create SDL window
+    window = SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_BORDERLESS);    
+    if(!window) {
+        std::cerr << "Error creating SDL window." << std::endl;
+        return;
+    }
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if(!renderer) {
+        std::cerr << "Error creating SDL renderer." << std::endl;
+    }
+
+    isRunning = true;
+    return;
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,18 @@
-#include <iostream>
+#include "Constants.h"
+#include "Game.h"
 
 int main(int argc, char *args[]) {
-    //TODO:
-    //Add game logic
+    Game *game = new Game();
 
-    std::cout << "Game is running..." << std::endl;
+    game->Initialize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    while(game->IsRunning()) {
+        game->ProcessInput();
+        game->Update();
+        game->Render();
+    }
+
+    game->Destroy();
+
     return 0;
 }
