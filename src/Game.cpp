acbb367f75c8cf0c4bb105ea41d5216847f1d0fc
mj <jelonek.martin@gmail.com>
2021-03-09 18:20:14 +0100

    Adding ProcesInput to the Game class.

--- src/Game.cpp
+++ src/Game.cpp
@@ -39,3 +39,18 @@ void Game::Initialize(int width, int height) {
     isRunning = true;
     return;
 }
+
+void Game::ProcessInput() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch (event.type)
+    {
+    case SDL_QUIT:
+        isRunning = false;
+        break;
+    case SDL_KEYDOWN:
+        if(event.key.keysym.sym == SDLK_ESCAPE) isRunning = false;
+    default:
+        break;
+    }
+}
