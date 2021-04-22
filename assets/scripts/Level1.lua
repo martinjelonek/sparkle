41551ad114f0bca9ef305b1f8aef39db4781fdec
mj <jelonek.martin@gmail.com>
2021-04-22 19:52:49 +0200

    Changed catapult to troops enemy, #34

--- assets/scripts/Level1.lua
+++ assets/scripts/Level1.lua
@@ -2,9 +2,10 @@ Level1 = {
     assets = {
         [0] = { type="texture", id = "terrain-fields", file = "./assets/tilemaps/fields.png"},
         [1] = { type="texture", id = "wildhammer", file = "./assets/images/wildhammer.png"},
-        [2] = { type="texture", id = "catapult", file = "./assets/images/catapult-big-right.png"},
-        [3] = { type="texture", id = "enemy-projectile", file = "./assets/images/enemy-projectile.png"},
-        [4] = { type="font", id = "pixeldown", file = "./assets/fonts/pixeldown.ttf", fontSize = 14}
+        [2] = { type="texture", id = "troops", file = "./assets/images/troops.png"},
+        [3] = { type="texture", id = "troops-moveing", file = "./assets/images/troops-moveing.png"},
+        [4] = { type="texture", id = "enemy-projectile", file = "./assets/images/enemy-projectile.png"},
+        [5] = { type="font", id = "pixeldown", file = "./assets/fonts/pixeldown.ttf", fontSize = 14}
     },
     map = {
         textureAssetId = "terrain-fields",
@@ -21,8 +22,8 @@ Level1 = {
             components = {
                 transform = {
                     position = {
-                        x = 150,
-                        y = 150
+                        x = 128,
+                        y = 64
                     },
                     velocity = {
                         x = 0,
@@ -56,13 +57,13 @@ Level1 = {
             }
         },
         [1] = {
-            name = "catapult-enemy-1",
+            name = "troops-enemy-1",
             layer = 2,
             components = {
                 transform = {
                     position = {
-                        x = 200,
-                        y = 200
+                        x = 192,
+                        y = 160
                     },
                     velocity = {
                         x = 20,
@@ -74,8 +75,11 @@ Level1 = {
                     rotation = 0
                 },
                 sprite = {
-                    textureAssetId = "catapult",
-                    animated = false,
+                    textureAssetId = "troops-moveing",
+                    animated = true,
+                    hasDirection = false;
+                    frameCount = 2,
+                    animationSpeed = 375,
                     fixed = false
                 },
                 collider = {
@@ -83,8 +87,8 @@ Level1 = {
                 },
                 projectileEmitter = {
                     textureAssetId = "enemy-projectile",
-                    width = 4,
-                    height = 4,
+                    width = 20,
+                    height = 20,
                     speed = 50,
                     range = 200,
                     angle = 275,
@@ -93,6 +97,43 @@ Level1 = {
             }
         },
         [2] = {
+            name = "troops-enemy-2",
+            layer = 2,
+            components = {
+                transform = {
+                    position = {
+                        x = 192,
+                        y = 192
+                    },
+                    velocity = {
+                        x = 0,
+                        y = 0,
+                    },
+                    width = 32,
+                    height = 32,
+                    scale = 1,
+                    rotation = 0
+                },
+                sprite = {
+                    textureAssetId = "troops",
+                    animated = false,
+                    fixed = false
+                },
+                collider = {
+                    tag = "enemy"
+                },
+                projectileEmitter = {
+                    textureAssetId = "enemy-projectile",
+                    width = 20,
+                    height = 20,
+                    speed = 50,
+                    range = 200,
+                    angle = 275,
+                    loop = true
+                }
+            }
+        },
+        [3] = {
             name = "label-title",
             layer = 5,
             components = {
